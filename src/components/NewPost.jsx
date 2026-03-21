814c1d313dd3b71a659a75942d781d78ec56c357
Harleen Kaur <hardhillon2013@gmail.com>
2026-03-20 19:11:46 -0500

    Updating stated based on previous states

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,15 +1,38 @@
+import { useState } from 'react';
 import classes from './NewPost.module.css';
 
-function NewPost({onNameChange, onDescriptionChange, onCancel}) {
+function NewPost({onCancel, onAddPost}) {
+  const [ enteredDescription, setEnteredDescription ] = useState('');
+    const [ enteredName, setEnteredName ] = useState('');
+
+//   define event handlers in your components to respond to these events. For example, you might create a function that gets executed when a user clicks a button or submits a form.
+   function changeDescriptionHandler(event){
+    setEnteredDescription(event.target.value);
+   }
+   function changeNameHandler(event){
+    setEnteredName(event.target.value);
+   }
+   function submitHandler(event){
+    event.preventDefault();
+    const postData = {
+      name: enteredName,
+      description: enteredDescription
+    };
+    onAddPost(postData);
+    onCancel();
+   }
+
+
   return (
-    <form className={classes.form}>
+
+    <form className={classes.form} onSubmit={submitHandler}>
       <p>
         <label htmlFor="name">Garden Name</label>
-        <input type="text" id="name" required onChange={onNameChange}/>
+        <input type="text" id="name" required onChange={changeNameHandler}/>
       </p>
       <p>
         <label htmlFor="body">Description</label>
-        <textarea id="body" required rows={3} onChange={onDescriptionChange}/>
+        <textarea id="body" required rows={3} onChange={changeDescriptionHandler}/>
       </p>
       <p className={classes.actions}>
         <button type="button" onClick={onCancel}>Cancel</button>
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import NewPost from './NewPost';
 import Post from './Post'
 import classes from './PostsList.module.css';
 import Modal from './Modal'
 
 function PostsList({isPosting, onStopPosting}){
-    
-    const [ enteredDescription, setEnteredDescription ] = useState('');
-    const [ enteredName, setEnteredName ] = useState('');
+    const [posts, setPosts] = useState([]);
 
-//   define event handlers in your components to respond to these events. For example, you might create a function that gets executed when a user clicks a button or submits a form.
-   function changeDescriptionHandler(event){
-    setEnteredDescription(event.target.value);
-   }
-   function changeNameHandler(event){
-    setEnteredName(event.target.value);
-   }
+    function addPostHandler(postData){
+        setPosts((existingPosts) => [postData, ...existingPosts]);
+    }
 
-   
     return (
         <>
         {isPosting ?
         <Modal onClose={onStopPosting}>
         <NewPost 
-        onNameChange ={changeNameHandler} 
-        onDescriptionChange={changeDescriptionHandler}
-        onCancel={onStopPosting}/> 
+        onCancel={onStopPosting}
+        onAddPost={addPostHandler}/> 
         </Modal> : false}
             <ul className={classes.posts}>
-                <Post garden={enteredName} description={enteredDescription} />
                 <Post garden="kildonan park" description="wonderful place to walk in greenery" />
             </ul>
             </>
